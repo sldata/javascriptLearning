fa190ccf3faa99eea10dec817a565d45e38afa4e
Sławomir <35075574+sldata@users.noreply.github.com>
2022-10-08 22:10:45 +0200

    variables - zmienne

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.body.style.backgroundColor = '#333'
+
+// ========================================
+
+// ZMIENNE
+
+// ========================================
+
+const firstName = 'Lisa'  
+// zmienna stała = nie zmienia się informacja np. imię
+
+console.log(firstName)
+
+let age = 23
+// zmienna = możemy dowolną ilość razy zmieniać zawartość tej zmiennej
+console.log(age);
+age = 45
+console.log(age);
+
+
